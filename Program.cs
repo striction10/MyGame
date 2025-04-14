a2c2df024d06d7798da0ebc5d9ad06b175339d52
Ilia <ilia_shcherbakov1234@mail.ru>
2025-04-14 19:58:08 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Start
+{
+    public static void Main(string[] args)
+    {
+        int count = 3;
+        Random rnd = new Random();
+        int value = rnd.Next(0, 10);
+        while (count > 0) { 
+            Console.Write("Введите число: ");
+            int value_user = Convert.ToInt32(Console.ReadLine());
+            count--;
+            if (value > value_user)
+            {
+                Console.WriteLine("Загаданное чилсо больше!");
+            }
+            else if (value < value_user)
+            {
+                Console.WriteLine("Загаданное число меньше!");
+            }
+            else if (value == value_user)
+            {
+                Console.WriteLine("Your is super puper!");
+                Console.WriteLine("Game over");
+                return;
+            }
+            if (count == 0) 
+            {
+                Console.WriteLine("Game over");
+                return;
+            }
+        }
+    }
+}
